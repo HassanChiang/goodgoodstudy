9f363547b520c2e3945bfde085df52f67fbb8640
Hassan <wysjhx@qq.com>
2020-04-12 15:48:53 +0800

    curator-recipes NodeCache test;

--- zookeeper/src/test/java/com/fenxiangz/learn/curator/CuratorOperatorTest.java
+++ zookeeper/src/test/java/com/fenxiangz/learn/curator/CuratorOperatorTest.java
@@ -2,8 +2,12 @@ package com.fenxiangz.learn.curator;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.CuratorWatcher;
+import org.apache.curator.framework.recipes.cache.NodeCache;
 import org.apache.curator.retry.RetryNTimes;
+import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.data.Stat;
 import org.junit.Assert;
 import org.junit.Before;
@@ -18,6 +22,7 @@ public class CuratorOperatorTest {
     private static CuratorFramework client = null;
     private static final String connectString = "localhost:2181";
     private static Watcher defaultWatcher;
+    private static CuratorWatcher curatorWatcher;
 
     @Before
     public void before() {
@@ -28,7 +33,13 @@ public class CuratorOperatorTest {
                 .sessionTimeoutMs(2000).build();
         client.start();
         defaultWatcher = event -> {
-            logger.info("trigger event:\n" +
+            logger.info("defaultWatcher trigger event:\n" +
+                    "{}\n" +
+                    "{}\n" +
+                    "{}", event.getPath(), event.getType(), event.getState());
+        };
+        curatorWatcher = event -> {
+            logger.info("curatorWatcher trigger event:\n" +
                     "{}\n" +
                     "{}\n" +
                     "{}", event.getPath(), event.getType(), event.getState());
@@ -46,8 +57,12 @@ public class CuratorOperatorTest {
         Stat stat = client.checkExists().usingWatcher(defaultWatcher).forPath(testPath);
         Assert.assertEquals(null, stat);
 
-        client.create().forPath(testPath, testData.getBytes());
-        stat = client.checkExists().watched().forPath(testPath);
+        client.create()
+                .creatingParentsIfNeeded()
+                .withMode(CreateMode.PERSISTENT)
+                .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
+                .forPath(testPath, testData.getBytes());
+        stat = client.checkExists().usingWatcher(defaultWatcher).forPath(testPath);
         Assert.assertNotNull(stat);
 
         byte[] bytes = client.getData().watched().forPath(testPath);
@@ -55,12 +70,51 @@ public class CuratorOperatorTest {
         Assert.assertEquals(testData, getData);
 
         client.setData().withVersion(0).forPath(testPath, testData2.getBytes());
-        bytes = client.getData().watched().forPath(testPath);
+        bytes = client.getData().usingWatcher(curatorWatcher).forPath(testPath);
         getData = new String(bytes);
         Assert.assertEquals(testData2, getData);
 
         client.delete().withVersion(1).forPath(testPath);
-        stat = client.checkExists().watched().forPath(testPath);
+        stat = checkExist(testPath);
         Assert.assertNull(stat);
+    }
+
+    @Test
+    public void nodeCacheTest() throws Exception {
+        String path = "/nodeCacheTest";
+        String testData = "nodeCacheTest";
+
+        NodeCache nodeCache = new NodeCache(client, path);
+        nodeCache.start(true);
+        nodeCache.getListenable().addListener(() -> {
+            logger.info("path {}, current data:{}", path, new String(nodeCache.getCurrentData().getData()));
+        });
+        Stat stat = checkExist(path);
+        if (stat == null) {
+            client.create().creatingParentsIfNeeded().forPath(path, testData.getBytes());
+        }
+        stat = checkExist(path);
+        Assert.assertNotNull(stat);
+        Assert.assertEquals(testData, getData(path));
+        for (int i = 0; i < 10; i++) {
+            String s = testData + i;
+            client.setData().forPath(path, s.getBytes());
+            Assert.assertEquals(s, getData(path));
+        }
+
+        stat = checkExist(path);
+        Assert.assertNotNull(stat);
+
+        client.delete().forPath(path);
+        stat = checkExist(path);
+        Assert.assertNull(stat);
+    }
+
+    private String getData(String path) throws Exception {
+        return new String(client.getData().forPath(path));
+    }
+
+    private Stat checkExist(String path) throws Exception {
+        return client.checkExists().forPath(path);
     }
 }
