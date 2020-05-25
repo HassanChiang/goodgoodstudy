c34cec1ac92512263110d5a2a026f9a60b0c3354
江浩翔 <jianghx@tech.youyuan.com>
2020-05-25 14:03:44 +0800

    LeetCode 1501;

---
+++ learn-gradle/src/main/java/com/fenxiangz/learn/leetcode/LeetCode1510.java
@@ -0,0 +1,77 @@
+package com.fenxiangz.learn.leetcode;
+
+
+/**
+ * 给定一个32位整数 num，你可以将一个数位从0变为1。请编写一个程序，找出你能够获得的最长的一串1的长度。
+ *
+ * 示例 1：
+ * 输入: num = 1775(110111011112)
+ * 输出: 8
+ *
+ * 示例 2：
+ * 输入: num = 7(01112)
+ * 输出: 4
+ *
+ */
+public class LeetCode1510 {
+    public static void main(String[] args) {
+        int result = new Solution2().reverseBits(7);
+        System.out.printf("result:" + result);
+    }
+}
+
+/**
+ * 常规思路
+ *
+ * 遍历每一位，统计“0分隔”开的前串长度和后串长度；
+ * 每次遇到0时，则将前串和后串相加再+1得到当前翻转的最大串（ maxPre + 1 + maxAfter）；
+ * 与 max 比较选出最大值保存即可。
+ */
+class Solution {
+    public int reverseBits(int num) {
+        String[] ss = Integer.toBinaryString(num).split("");
+        int max = 0;
+        // 0标志前串
+        int maxPre = 0;
+        // 0标志后串
+        int maxAfter = 0;
+        for (int i = 0; i < ss.length; i++) {
+            if ("0".equals(ss[i])) {
+                max = Math.max(max, maxPre + 1 + maxAfter);
+                maxPre = maxAfter;
+                maxAfter = 0;
+            } else {
+                maxAfter++;
+            }
+        }
+        max = Math.max(max, maxPre + 1 + maxAfter);
+        return max;
+    }
+}
+
+/**
+ * 思路不变，通过位运算实现，提高执行效率
+ */
+class Solution2 {
+    public int reverseBits(int num) {
+        int bits = 32;
+        int max = 0;
+        // 0标志前串
+        int maxPre = 0;
+        // 0标志后串
+        int maxAfter = 0;
+
+        while (bits-- > 0) {
+            if ((num & 1) == 0) {
+                max = Math.max(max, maxPre + 1 + maxAfter);
+                maxPre = maxAfter;
+                maxAfter = 0;
+            } else {
+                maxAfter++;
+            }
+            num >>= 1;
+        }
+        max = Math.max(max, maxPre + 1 + maxAfter);
+        return max;
+    }
+}
