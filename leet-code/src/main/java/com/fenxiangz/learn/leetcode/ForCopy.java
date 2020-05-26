a9f1e6297c7093fbf5d14b7443b0da2d61eb2234
江浩翔 <jianghx@tech.youyuan.com>
2020-05-26 14:59:19 +0800

    LeetCode 287;

---
+++ leet-code/src/main/java/com/fenxiangz/learn/leetcode/ForCopy.java
@@ -0,0 +1,20 @@
+package com.fenxiangz.learn.leetcode;
+
+import com.alibaba.fastjson.JSON;
+
+public class ForCopy {
+    public static void main(String[] args) {
+        int result = new Solution2().method();
+        System.out.printf("result:" + JSON.toJSONString(result));
+    }
+
+    private static class Solution1 {
+
+    }
+
+    private static class Solution2 {
+        public int method() {
+            return 0;
+        }
+    }
+}
