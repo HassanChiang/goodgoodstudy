163e141e533805a94d8c4e578c53360f13dbe244
Hassan <wysjhx@qq.com>
2020-07-26 18:26:36 +0800

    学习模板语法

--- vue/hello-world/src/components/HelloWorld.vue
+++ vue/hello-world/src/components/HelloWorld.vue
@@ -1,58 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <h1>{{ hello }}</h1>
+        <span v-html="rawHtml"></span>
+        <div v-bind:id="dynamicId">
+            div id attribute = dynamicId111
+        </div>
+
+        <button v-bind:disabled="buttonStatus.btn1">disabled=null</button>
+        <button v-bind:disabled="buttonStatus.btn2">disabled=undefined</button>
+        <button v-bind:disabled="buttonStatus.btn3">disabled=false</button>
+        <button v-bind:disabled="buttonStatus.btn4">disabled=1</button>
+        <button v-bind:disabled="buttonStatus.btn5">disabled=true</button>
+
+        <p v-if="pSeen">现在你看到我了</p>
+        <p v-if="pSeen2 == 6">现在你看到我了（pSeen2 == 6）</p>
+
+        <div class="testA">
+            <a v-bind:href="fenxiangz" target="_blank">fenxiangz.com</a>
+            <a :href="fenxiangz" target="_blank">fenxiangz.com</a>
+
+            <a v-on:click="showAlert('HHHH')">alert('HHHH')</a>
+            <a @click="showAlert('HHHH')">alert('HHHH')</a>
+        </div>
+
+
+        <h1>翻转消息： {{ reverseMsg }}</h1>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                hello: "000",
+                rawHtml: '<balel style="color: red">AAAAA</balel>',
+                dynamicId: 'dynamicId111',
+                buttonStatus: {
+                    btn1: null,
+                    btn3: false,
+                    btn4: 1,
+                    btn5: true,
+                },
+                pSeen: true,
+                pSeen2: 6,
+                fenxiangz: 'https://www.fenxiangz.com',
+
+            }
+        },
+        created() {
+            this.hello = "Hello, Hassan"
+        },
+        methods: {
+            showAlert: function (msg) {
+                window.alert(msg);
+            }
+
+        },
+        computed:{
+            reverseMsg: function () {
+                return this.hello.split("").reverse().join("");
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+        display: block;
+        margin: 5px 5px;
+    }
+
+    .testA > a {
+        display: inline-block;
+    }
+
+    button {
+        margin: 5px 5px;
+    }
+
+    div {
+        margin: 5px 5px;
+    }
 </style>
